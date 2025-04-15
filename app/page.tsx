fe9a7f1eef2c994440b11c92ba840bcb75fe351b
MrRedeks <m2208442@edu.misis.ru>
2025-04-16 02:43:47 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,8 @@ import AuthForm from "@/components/AuthForm";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
-=======
     <div className="w-full min-h-screen flex items-center justify-center bg-gray-50 px-4">
-      <div className="">
->>>>>>> d0cb840 (подогнал сайт под дизайн + сделал кнопки для входа/регистрации/выхода)
       <AuthForm />
-      </div>
     </div>
   );
 }
